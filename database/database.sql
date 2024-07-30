3a6aa64282bf4a7cf2ad07c0fbb01b4a48033484
Jade Aidoghie <120065528+Jade010@users.noreply.github.com>
2024-07-30 10:07:36 -0700

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,9 @@
+CREATE DATABASE CookingCompanionDb;
+GO
+
+USE CookingCompanionDb;
+GO
+
 -- Users table
 CREATE TABLE Users (
     UserId INT PRIMARY KEY IDENTITY, -- auto-increment
@@ -48,96 +54,3 @@ INSERT INTO Categories (Name, Subcategories) VALUES
 ('Preparation Method', '["Baking", "Grilling", "Roasting", "Stir-Frying", "Slow Cooking", "Pressure Cooking", "No-Cook"]'),
 ('Occasions', '["Holiday", "Party", "Family Gatherings", "Quick & Easy", "Comfort Food", "Healthy"]'),
 ('Health & Lifestyle', '["Low-Fat", "Low-Sodium", "High-Protein", "Sugar-Free", "Heart-Healthy"]');
-
-
-
--- Triggers
-
--- Password complexity
-CREATE TRIGGER trg_CheckPasswordComplexity
-ON Users
-FOR INSERT, UPDATE
-AS
-BEGIN
-    DECLARE @Password NVARCHAR(255)
-    SELECT @Password = inserted.PasswordHash
-    FROM inserted
-
-    IF NOT (
-        @Password LIKE '%[A-Z]%' AND
-        @Password LIKE '%[a-z]%' AND
-        @Password LIKE '%[0-9]%' AND
-        @Password LIKE '%[!@#$%^&*()]%'
-    )
-    BEGIN
-        ROLLBACK;
-        RAISERROR ('Password does not meet requirements.', 16, 1);
-    END
-END;
-
-
--- Updating recipe timestamp 
-CREATE TRIGGER trg_UpdateRecipeTimestamp
-ON Recipes
-FOR UPDATE
-AS
-BEGIN
-    UPDATE Recipes
-    SET Date = GETDATE()
-    FROM inserted
-    WHERE Recipes.RecipeId = inserted.RecipeId;
-END;
-
-
--- No repeated categories 
-CREATE TRIGGER trg_UniqueCategoryName
-ON Categories
-FOR INSERT, UPDATE
-AS
-BEGIN
-    DECLARE @CategoryName NVARCHAR(50)
-    SELECT @CategoryName = inserted.Name
-    FROM inserted
-
-    IF EXISTS (SELECT 1 FROM Categories WHERE Name = @CategoryName)
-    BEGIN
-        ROLLBACK;
-        RAISERROR ('Category name must be unique.', 16, 1);
-    END
-END;
-
-
--- User roles
-CREATE TRIGGER trg_EnforceUserRole
-ON Users
-FOR INSERT, UPDATE
-AS
-BEGIN
-    -- Declaring variables to hold inserted values
-    DECLARE @Email NVARCHAR(50);
-    DECLARE @Role NVARCHAR(20);
-
-    -- Selecting the inserted values
-    SELECT @Email = inserted.Email, @Role = inserted.Role
-    FROM inserted;
-
-    -- Check if the inserted/updated user is the admin account
-    IF @Email = 'admin@example.com'
-    BEGIN
-        -- Allow the admin account to retain the "admin" role
-        IF @Role != 'admin'
-        BEGIN
-            -- Revert to "admin" role if any other role is set
-            UPDATE Users
-            SET Role = 'admin'
-            WHERE Email = @Email;
-        END
-    END
-    ELSE
-    BEGIN
-        -- For all other users, enforce "regular" role
-        UPDATE Users
-        SET Role = 'regular'
-        WHERE Email = @Email;
-    END
-END;
